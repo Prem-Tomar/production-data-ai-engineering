c479f587b0163b47e1a8dcaf17d3d72afcb73ebb
chouhansujeet20-ctrl <chouhansujeet20@gmail.com>
2026-09-20 20:39:35 +0530

    Create Day1CalculateTradeAmt.py
    Added code for day1

---
+++ labs/01-reliable-ingestion/Day1CalculateTradeAmt.py
@@ -0,0 +1,12 @@
+quantity = 3
+price = 10
+result = quantity * price
+print(result)
+quantity = 5
+price = 10
+result = quantity * price
+print(result)
+quantity = 2
+prince = 7
+result = quantity * price
+print(result)
