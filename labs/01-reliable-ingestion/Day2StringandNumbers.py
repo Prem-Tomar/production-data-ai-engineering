25ad4bb5845f140834cafe6e485318512a008aa7
Sujeet Chouhan <sujeetchouhan@Sujeets-MacBook-Air.local>
2026-09-24 12:05:05 +0530

    adding file for day 2 having string and string converion to int

---
+++ labs/01-reliable-ingestion/Day2StringandNumbers.py
@@ -0,0 +1,13 @@
+int1 = 3
+int2 = 2
+result = int1+int2
+print(result)
+str1="3"
+str2="2"
+result = str1+str2
+print(result)
+quantity = "4"
+result = int(quantity)*10
+print(result)
+str3 = int("4")
+print(str3)
